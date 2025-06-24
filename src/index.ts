a620d811e239c9a3da7669cbe8b382ce4a64391e
Yasser <yassercomunica@gmail.com>
2025-06-24 11:57:47 -0600

    Deployment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { PrismaClient } from '@prisma/client';
-import path from 'path';
-import AppProduct from './routes/index';
-import cors from 'cors';
-import authRouter from './routes/auth/auth.route';  
-import expressOasGenerator from 'express-oas-generator';
-import paypalRouter from './routes/payments/checkout.route';
-import orderRoute from './routes/orders/orders.route';
+import express from "express";
+import bodyParser from "body-parser";
+import { PrismaClient } from "@prisma/client";
+import path from "path";
+import AppProduct from "./routes/index";
+import cors from "cors";
+import authRouter from "./routes/auth/auth.route";
+import expressOasGenerator from "express-oas-generator";
+import paypalRouter from "./routes/payments/checkout.route";
+import orderRoute from "./routes/orders/orders.route";
 
 const app = express();
 
@@ -20,27 +20,33 @@ app.use(bodyParser.json());
 
 export const prismaclient = new PrismaClient();
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api', AppProduct);
-app.use('/api/auth', authRouter);
-app.use('/api/payments', paypalRouter);
-app.use('/api/orders', orderRoute);
+app.use("/api", AppProduct);
+app.use("/api/auth", authRouter);
+app.use("/api/payments", paypalRouter);
+app.use("/api/orders", orderRoute);
 
-app.get('/', (_req, res) => {
-  res.send('API is running');
+app.get("/", (_req, res) => {
+  res.send("API is running");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+console.log("🚀 PORT from env:", process.env.PORT);
+if (!PORT) {
+  console.error("PORT env variable not set.");
+  process.exit(1);
+}
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`✅ App listening on port ${PORT}`);
   console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
 
 
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
 });
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason);
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection:", reason);
 });
