520ebab63f4d094e5a6ac48cb8a0a191cdbb2d30
Yasser <yassercomunica@gmail.com>
2025-06-24 11:30:05 -0600

    Deployment

--- build/src/schema/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.schemaProduct = void 0;
-const zod_1 = require("zod");
-exports.schemaProduct = zod_1.z.object({
-    name: zod_1.z.string(),
-    categoryId: zod_1.z.number()
-});
--- build/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require('dotenv').config();
-console.log({
-    nodeVersion: process.version,
-    paypalId: process.env.PAYPAL_CLIENT_ID ? 'Existe' : 'No existe',
-    envKeys: Object.keys(process.env).filter(k => k.includes('PAYPAL'))
-});
